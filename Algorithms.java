da16844240b30723a18f1a972ceac5d3487ad8e9
rachelw106 <rachelw106@nycstudents.net>
2024-12-04 03:46:29 +0000

    commit

--- Algorithms.java
+++ Algorithms.java
@@ -9,6 +9,12 @@ public class Algorithms {
         f = new File("Numbers.txt");
         int odds = odds();
         int evens = evens();
+        int twoDigit = twoDigit();
+        int greater = greater();
+        int greatest = greatest();
+        int least = least();
+        int sum = sum();
+        int avg = sum / numofnums();
         System.out.println(odds);
         s.close();
     }
@@ -31,5 +37,62 @@ public class Algorithms {
                 evens++;
         }
         return evens;
+    }
+    public static int twoDigit() throws FileNotFoundException{
+        s = new Scanner(f);
+        int twoDigit = 0;
+        while (s.hasNext()) {
+            int n = s.nextInt();
+            if (n % 10 < 10 && n % 10 >= 1)
+                twoDigit++;
+        }
+        return twoDigit;
+    }
+    public static int greater() throws FileNotFoundException{
+        s = new Scanner(f);
+        int greater = 0;
+        while (s.hasNext()) {
+            if (s.nextInt() > 500)
+                greater++;
+        }
+        return greater;
+    }
+    public static int greatest() throws FileNotFoundException{
+        s = new Scanner(f);
+        int greatest = 0;
+        int prev = 0;
+        while (s.hasNext()) {
+            int n = s.nextInt();
+            if (n > prev) greatest = n;
+            prev = n;
+        }
+        return greatest;
+    }
+    public static int least() throws FileNotFoundException{
+        s = new Scanner(f);
+        int least = 0;
+        int prev = 0;
+        while (s.hasNext()) {
+            int n = s.nextInt();
+            if (n < prev) least = n;
+            prev = n;
+        }
+        return least;
+    }
+    public static int sum() throws FileNotFoundException{
+        s = new Scanner(f);
+        int sum = 0;
+        while (s.hasNext()) {
+            sum += s.nextInt();
+        }
+        return sum;
+    }
+    public static int numofnums() throws FileNotFoundException{
+        s = new Scanner(f);
+        int num = 0;
+        while (s.hasNext()) {
+            num++;
+        }
+        return num;
     }
 }
