91ecebe825d0ed801d37111ee9365d1ea1cf9fb4
rachelw106 <rachelw106@nycstudents.net>
2024-12-11 14:19:16 +0000

    commit

--- Algorithms.java
+++ Algorithms.java
@@ -4,38 +4,72 @@ import java.io.FileNotFoundException;
 
 public class Algorithms {
     private static File f;
+    private static File g;
     private static Scanner s, ss;
     public static void main(String[] args) throws FileNotFoundException {
+        g = new File("words.txt");
+        System.out.println("Two Letter Words: " + twoLetter());
+        System.out.println("Number of Longest Words: " + longest());
+        System.out.println("Palindromes: " + palindromes());
+
+
         f = new File("Numbers.txt");
-        int odds = odds();
-        System.out.println("odds: " + odds);
-
-        int evens = evens();
-        System.out.println("evens: " + evens);
-
-        int twoDigit = twoDigit();
-        System.out.println("Two Digit Numbers: " + twoDigit);
-
-        int greater = greater();
-        System.out.println("Greater than 500:" + greater);
-
-        int greatest = greatest();
-        System.out.println("Greatest: " + greatest);
-
-        int least = least();
-        System.out.println("Least: " + least);
-
-        int sum = sum();
-        System.out.println("Sum: " + sum);
-
-        double avg = (double) sum / (odds+evens);
+        System.out.println("odds: " + odds());
+        System.out.println("evens: " + evens());
+        System.out.println("Two Digit Numbers: " + twoDigit());
+        System.out.println("Greater than 500:" + greater());
+        System.out.println("Greatest: " + greatest());
+        System.out.println("Least: " + least());
+        System.out.println("Sum: " + sum());
+        double avg = (double) sum() / (odds()+evens());
         System.out.println("Average: " + avg);
-
-        int mode = mode();
-        System.out.println("Mode: " + mode);
+        System.out.println("Mode: " + mode());
 
         s.close();
     }
+
+    public static int twoLetter() throws FileNotFoundException{
+        s = new Scanner(g);
+        int two = 0;
+        while (s.hasNextLine()) {
+            String str = s.nextLine();
+            if(str.length() == 2) two++;
+        }
+        return two;
+    }
+    public static int longest() throws FileNotFoundException{
+        s = new Scanner(g);
+        ss = new Scanner(g);
+        int longest = 0;
+        while (s.hasNextLine()) {
+            String str = s.nextLine();
+            if(str.length() > longest) longest = str.length();
+        }
+        int count = 0;
+        while(ss.hasNextLine()) {
+            String str = ss.nextLine();
+            if(str.length() == longest) count++;
+        }
+        return count;
+    }
+    public static int palindromes() throws FileNotFoundException{
+        s = new Scanner(g);
+        int a = 0;
+        while (s.hasNextLine()) {
+            String str = s.nextLine();
+            int len = str.length();
+            int count = 0;
+            for(int i = 0; i < len/2; i++) {
+                if(str.substring(i,i+1).equals(str.substring(len-i-1,len-i))) count++;
+            }
+            if(count == len/2) a++;
+        }
+        return a;
+    }
+
+
+
+
 
     public static int odds() throws FileNotFoundException{
         s = new Scanner(f);
@@ -100,18 +134,23 @@ public class Algorithms {
         }
         return sum;
     }
-    public static int mode() throws FileNotFoundException{
+    public static int mode() throws FileNotFoundException
+    {
         s = new Scanner(f);
-        ss = new Scanner(f);
+        int[] nums = new int[1000];
+        while(s.hasNextInt())
+        {
+            nums[s.nextInt()]++;
+        }
+        int max = nums[0];
         int mode = 0;
-        while (s.hasNext()) {
-            int count = 0;
-            int n = s.nextInt();
-            while(ss.hasNext()) {
-                int m = ss.nextInt();
-                if(n == m) count++;
+        for(int i = 1; i < nums.length; i++)
+        {
+            if (nums[i] > max)
+            {
+                mode = i;
+                max = nums[i];
             }
-            if(count >= mode) mode = n;
         }
         return mode;
     }
